76629aa6322368e00b81c3e95a7faf7e43822259
devAon <cyw9742@naver.com>
2019-07-02 21:10:11 +0900

    카테고리 해시태그 작성

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/users', require('./user/index'));
-router.use('/posts', require('./post/index'));
 router.use('/boards', require('./board/index'));
 router.use('/creators', require('./creator/index'));
 router.use('/votes', require('./vote/index'));
-
+router.use('/hashtags', require('./hashtags/index'));
+router.use('/categories', require('./categories/index'));
 
 module.exports = router;
